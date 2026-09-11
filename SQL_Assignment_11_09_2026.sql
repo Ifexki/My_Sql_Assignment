df0a95648874eba72168780a6edbc02b4c775bbb
Ifexki <66957856+Ifexki@users.noreply.github.com>
2026-09-11 16:48:02 +0100

    My Assignment

---
+++ SQL_Assignment_11_09_2026.sql
@@ -0,0 +1,9 @@
+--Create Database PortOps
+
+USE PortOps;
+GO
+
+-- ============================================================
+--  TABLE 1: Vessels
+--  Represents ships calling at the terminal
+-- ============================================================
